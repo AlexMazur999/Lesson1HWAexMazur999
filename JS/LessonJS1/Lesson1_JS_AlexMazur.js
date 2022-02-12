cdf26a0412f6d99b237f1043e838b0c5263f401c
AlexMazur999 <Alex.mazur999@gmail.com>
2022-02-12 17:04:49 +0200

    Add home work JS Lesson1

---
+++ JS/LessonJS1/Lesson1_JS_AlexMazur.js
@@ -0,0 +1,189 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+let message = `hello`;
+let school = `owu`;
+let site = `com`;
+let country = `ua`;
+let number1 = 1;
+let number10 = 10;
+let number999 = -999;
+let number123 = 123;
+const PI = 3.14;
+let number3 = 2.7;
+let number16 = 16;
+let pravda = `true`;
+let brehnja = `false`;
+console.log(message);
+console.log(school);
+console.log(site);
+console.log(country);
+console.log(number1);
+console.log(number10);
+console.log(number999);
+console.log(number123);
+console.log(PI);
+console.log(number3);
+console.log(number16);
+console.log(pravda);
+console.log(brehnja);
+// //
+// // // alert(message);
+// // // alert(school);
+// // // alert(site);
+// // // alert(country);
+// // // alert(number1);
+// // // alert(number10);
+// // // alert(number999);
+// // // alert(number123);
+// // // alert(PI);
+// // // alert(number3);
+// // // alert(number16);
+// // // alert(pravda);
+// // // alert(brehnja);
+//
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+document.write(`<p>${message}</p>`)
+document.write(`<p>${school}</p>`)
+document.write(`<p>${site}</p>`)
+document.write(`<p>${country}</p>`)
+document.write(`<p>${number1}</p>`)
+document.write(`<p>${number10}</p>`)
+document.write(`<p>${number999}</p>`)
+document.write(`<p>${number123}</p>`)
+document.write(`<p>${PI}</p>`)
+document.write(`<p>${number3}</p>`)
+document.write(`<p>${number16}</p>`)
+document.write(`<p>${pravda}</p>`)
+document.write(`<p>${brehnja}</p>`)
+
+
+let message2 = `yo man`;
+let school2 = `hight2`;
+let site2 = `lan`;
+let country2 = `usa`;
+let number11 = 11;
+let number110 = 110;
+let number888 = -888;
+let number321 = 321;
+const PI2 = 3.18;
+let number4 = 3.7;
+let number116 = 116;
+let pravda2 = `true2`;
+let brehnja2 = `false2`;
+console.log(message2);
+console.log(school2);
+console.log(site2);
+console.log(country2);
+console.log(number11);
+console.log(number110);
+console.log(number888);
+console.log(number321);
+console.log(PI2);
+console.log(number4);
+console.log(number116);
+console.log(pravda2);
+console.log(brehnja2);
+
+// alert(message2);
+// alert(school2);
+// alert(site2);
+// alert(country2);
+// alert(number11);
+// alert(number110);
+// alert(number888);
+// alert(number321);
+// alert(PI);
+// alert(number4);
+// alert(number116);
+// alert(pravda2);
+// alert(brehnja2);
+document.write(`<p>yo man</p>`)
+document.write(`<p>hight2</p>`)
+document.write(`<p>lan</p>`)
+document.write(`<p>usa</p>`)
+document.write(`<p>11</p>`)
+document.write(`<p>110</p>`)
+document.write(`<p>-888</p>`)
+document.write(`<p>321</p>`)
+document.write(`<p>3.18</p>`)
+document.write(`<p>3.7</p>`)
+document.write(`<p>116</p>`)
+document.write(`<p>true2</p>`)
+document.write(`<p>false2</p>`)
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+let person = {firstName: `Alex`, middleName: `Volodimirovich`, lastname: `Mazur` }
+console.log(person)
+let firstName = `Alex`
+let middleName = `Volodimirovich`
+let lastname = `Mazur`
+let person2 = (firstName + ' ' + middleName + ' ' + lastname)
+
+console.log(person2)
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100
+let b = `100`
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+// В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+
+console.log(5<6)
+console.log(5>6)
+console.log(5>=6)
+console.log(5==6)
+console.log(10==10)
+console.log(10<=10)
+console.log(10<10)
+console.log(10>10)
+console.log(10!=10)
+console.log(123==`123`)
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+let str = "20"
+let a1 = 5
+document.write(str + a1 + "<br/>")
+document.write(str - a1 + "<br/>")
+document.write(str * "2" + "<br/>")
+document.write(str / 2 + "<br/>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
